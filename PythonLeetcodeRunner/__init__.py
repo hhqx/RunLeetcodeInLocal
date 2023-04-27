8204e9647f9b14def952971fd284817907f589a8
std_huangqianxin <weiwushaonian@foxmail.com>
2023-04-27 18:17:10 +0800

    Add default import packages.

--- PythonLeetcodeRunner/__init__.py
+++ PythonLeetcodeRunner/__init__.py
@@ -6,26 +6,10 @@ from .def_List import ListNode
 
 from .def_Tree import TreeNode
 
-# customed tools for debug
-# import matplotlib.pyplot as plt
-# import numpy as np
+# 导入标准库, 和自定义的包
+from .load_packages import *
 
-# def plot_text(coords, sz=5):
-#     def add(x, y, str):
-#         plt.text(x, y, str, size=sz, rotation=0.,
-#                  ha="center", va="center",
-#                  bbox=dict(boxstyle="round",
-#                            ec=(1., 0.5, 0.5),
-#                            fc=(1., 0.8, 0.8),
-#                            )
-#                  )
-#
-#     plt.figure()
-#     for i, (x, y) in enumerate(coords):
-#         add(x, y, f'No.{i}: ({x}, {y})')
-#
-#     mx, mn = np.array(coords).max(axis=0), np.array(coords).min(axis=0)
-#     plt.axis([mn[0], mx[0], mn[1], mx[1]])
-#
-#     plt.show()
+import importlib
+if importlib.find_loader('visualize_tools'):
+    from visualize_tools import *
 
